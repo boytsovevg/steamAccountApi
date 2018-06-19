b19d6760fde8f4604e96300aa22f27451eaafde1
Eugene <evgeniy.boytsov@gmail.com>
2018-06-19 15:01:45 +0300

    add express types

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
+import { Request, Response, Router } from 'express';
 import accountService from './account.service';
 import { IAccountService } from '../interfaces';
 
 class AccountController {
     private accountService: IAccountService;
-    public router = express.Router();
+    public router: Router = express.Router();
 
     constructor() {
         this.accountService = accountService;
@@ -13,12 +14,12 @@ class AccountController {
 
     private _registerRoutes() {
 
-        this.router.get('/getAccountByName', async (req, res) => {
+        this.router.get('/getAccountByName', async (req: Request, res: Response) => {
             const account = await this.accountService.getAccountByNameAsync(req.query.name);
             return res.status(200).send(account);
         });
 
-        this.router.get('/getAccountGames', async (req, res) => {
+        this.router.get('/getAccountGames', async (req: Request, res: Response) => {
             const games = await this.accountService.getAccountGamesAsync(req.query.id);
 
             return res.status(200).send(games);
--- src/steam/steam.service.ts
+++ src/steam/steam.service.ts
@@ -1,4 +1,4 @@
-import { ISteamService, IAccount } from '../interfaces';
+import { ISteamService, IAccount, IGame } from '../interfaces';
 import * as http from 'axios';
 
 const API_KEY = '7D5F2FA02FF09ACA687DE979BE355B30';
@@ -13,19 +13,22 @@ class SteamService implements ISteamService {
     }
 
     public async getAccountIdByNameAsync(name: string): Promise<string> {
-        const response = await this.http.get(`${this.baseUrl}/ISteamUser/ResolveVanityURL/v1/?key=${API_KEY}&vanityurl=${name}`);
+        const response = await this.http.get(`${this.baseUrl}/ISteamUser/ResolveVanityURL/v1/
+            ?key=${API_KEY}&vanityurl=${name}`);
+
         return response.data.response.steamid;
     }
 
     public async getAccountAsync(accountId: string): Promise<IAccount> {
-        const response = await this.http.get(`${this.baseUrl}/ISteamUser/GetPlayerSummaries/v2/?key=${API_KEY}&steamids=${accountId}`);
+        const response = await this.http.get(`${this.baseUrl}/ISteamUser/GetPlayerSummaries/v2/
+            ?key=${API_KEY}&steamids=${accountId}`);
 
         return response.data.response.players[0];
     }
 
-    public async getAccountGamesAsync(accountId: string): Promise<any[]> {
-        const response = await this.http
-            .get(`${this.baseUrl}/IPlayerService/GetOwnedGames/v1/?key=${API_KEY}&steamid=${accountId}&include_appinfo=1`);
+    public async getAccountGamesAsync(accountId: string): Promise<IGame[]> {
+        const response = await this.http.get(`${this.baseUrl}/IPlayerService/GetOwnedGames/v1/
+                ?key=${API_KEY}&steamid=${accountId}&include_appinfo=1`);
 
         return response.data.response.games;
     }
